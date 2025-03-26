0ad7546117a76d1f4922a7996ef706ca88d6a853
Monique <moniquejroliveira@gmail.com>
2025-03-26 13:15:51 -0300

    Usando variaveis

--- Variavel.c
+++ Variavel.c
@@ -3,12 +3,12 @@
 int main ()
 {
     int minhaIdade;
-    int suaIdade;
-
     minhaIdade = 30;
-    suaIdade = 10;
 
-    printf("Minha idade é = %i.\nSua idade é =%i.\n", minhaIdade, suaIdade);
+    int suaIdade = 10;
+    int nossaIdadde = minhaIdade + suaIdade;
+
+    printf("Minha idade é = %i.\nSua idade é = %i.\nA soma das nossas idades é igual a = %i\n", minhaIdade, suaIdade, nossaIdadde);
 
     return 0;
 }
