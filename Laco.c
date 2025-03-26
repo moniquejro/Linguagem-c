5124adcf0c7afbb378adac6ae2031a03269f44a5
Monique <moniquejroliveira@gmail.com>
2025-03-26 19:19:37 -0300

    Laço e contador

---
+++ Laco.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+{
+    int count;
+    int num = 1;
+
+    for (count = 1; count <= 10; count++) //++ incrementa +1 sempre
+    {
+        printf("teste %i.\n", count);
+    }
+    
+
+    return 0;
+}
