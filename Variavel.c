2cecbaf2bd6f08b74ef91975f73101a0a1d7657c
Monique <moniquejroliveira@gmail.com>
2025-03-26 13:03:28 -0300

    Criando variaveis

---
+++ Variavel.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+    int minhaIdade;
+    int suaIdade;
+
+    minhaIdade = 30;
+    suaIdade = 10;
+
+    printf("Minha idade é = %i.\nSua idade é =%i.\n", minhaIdade, suaIdade);
+
+    return 0;
+}
