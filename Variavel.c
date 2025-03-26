dce64f37c689bf9e3157f15f5bfb0dac69c6cd6b
Monique <moniquejroliveira@gmail.com>
2025-03-26 16:15:15 -0300

    operaçoes matematicas

--- Variavel.c
+++ Variavel.c
@@ -2,13 +2,13 @@
 
 int main ()
 {
-    int minhaIdade;
-    minhaIdade = 30;
-
+    int minhaIdade = 30;
     int suaIdade = 10;
-    int nossaIdadde = minhaIdade + suaIdade;
+    int diferencaIdade = minhaIdade - suaIdade;
+    int somaIdade = minhaIdade + suaIdade;
+    
 
-    printf("Minha idade é = %i.\nSua idade é = %i.\nA soma das nossas idades é igual a = %i\n", minhaIdade, suaIdade, nossaIdadde);
+    printf("Minha idade é = %i.\nSua idade é = %i.\nA diferença das nossas idaddes é = %i.\nA soma das nossas idades é igual a = %i.\n", minhaIdade, suaIdade, diferencaIdade, somaIdade);
 
     return 0;
 }
