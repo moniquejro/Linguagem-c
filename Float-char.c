5330c6ee5d05dbfe72d5a58e8a06a92ccd0d4884
Monique <moniquejroliveira@gmail.com>
2025-03-26 17:31:35 -0300

    Conhecendo outras variaveis

---
+++ Float-char.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+{
+
+    int varint = 10;
+    float varfloat = 10.5;
+    char varchar = 'F';
+    
+    printf("O inteiro é: %i.\n",varint);
+    printf("O float é: %f.\n", varfloat);
+    print("O char é: %c.\n", varchar);
+
+    return 0;
+}
