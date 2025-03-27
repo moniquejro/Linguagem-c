c088b0488fd44964285df09d426a34b484ac0e3f
Monique <moniquejroliveira@gmail.com>
2025-03-27 10:23:09 -0300

    Analise de financiamento bancario

---
+++ Financiamento.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ()
+{
+
+    float casa;
+    float salario;
+    int anos;
+
+    scanf("Qual o valor da casa: R$:%f", &casa);
+    scanf("Qual o salario da pessoa: R$%f", salario);
+    scanf("Quantos anos de financianciamento: R$%i", anos);
+
+
+    return 0;
+}
