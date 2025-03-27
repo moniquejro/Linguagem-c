cd897ecd2966c4199d3c99ec1ae7cf945f7eec0c
Monique <moniquejroliveira@gmail.com>
2025-03-27 17:53:25 -0300

    cadastro final

--- Financiamento.c
+++ Financiamento.c
@@ -3,10 +3,21 @@
 int main ()
 {
 
+    /*
+    Escrever um programa para aprovar emprestimo bancario para a compra de uma casa. Pergunte o valor da casa, o salario da pessoa e em quantos anos pretende pagar. 
+    A prestaçao mensal nao pode exceder 30% do salario, caso contario o emprestimo é negado.
+    */
+
+    char nome[30];
     float casa;
     float salario;
     int anos;
     int meses;
+    float prestacao;
+    float minimo;
+
+    printf("Informe seu nome:\n");
+    scanf("%s", nome);
 
     printf("\nQual o valor da casa:\n");
     scanf("%f", &casa);
@@ -20,8 +31,20 @@ int main ()
     scanf("%i", &anos);
 
     meses = anos * 12;
+    prestacao = casa / (anos * 12); //encontrar valor prestaçao, quantos meses pagando 
+    minimo = 30 * (salario / 100); //encontrar valor teto da prestaçao de 30% salario
 
-    printf("Anos pagando: %i\nMeses pagando: %i\n", anos, meses);
+    printf("Seu nome é %s.\nAnos pagando: %i\nMeses pagando: %i\nValor prestaçao: R$ %f\nO minimo do salario é: %f\n", nome, anos, meses, prestacao, minimo);
+
+    if (prestacao <= minimo)
+    {
+        printf("O emprestimo foi liberado!\nA prestaçao é no valor de: %f sendo paga em %i meses.\n", prestacao, meses);
+    }
+    else 
+    {
+        printf("O emprestimo NAO foi aprovado. \nA parcela de R$ %f é MAIOR que o minimo que poderia ser utilizado do salario, que é de R$ %f.\n",prestacao, minimo);
+    }
+    
 
 
     return 0;
