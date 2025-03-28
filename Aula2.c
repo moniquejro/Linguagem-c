190dbeede32020e788c7e7df581b1aaa63be7748
Monique <moniquejroliveira@gmail.com>
2025-03-28 11:10:04 -0300

    Jogo qual num é maior? do while, if else

--- Aula2.c
+++ Aula2.c
@@ -16,15 +16,15 @@ int main ()
         printf("\nEscolha outro numero:\n");
         scanf("%i", &num2);
 
-        if (num1 > num2)
+        if (num1 > num2) //bloco que vai executar se for verdade
         {
             printf("\n[---- O primeiro numero é maior! ----]\n");
-        }
-        if (num1 == num2) 
+        } 
+        else if (num1 == num2) //bloco que vai executar se a condiçao1 for falsa e condiçao2 (essa) for verdade
         {
             printf("\n[---- Os numeros sao iguais! ----]\n");
-        }
-        else
+        } 
+        else //bloco que vai executar se as condiçoes anteriores forem falsas
         {
             printf("\n[---- O segundo numero é maior! ----]\n");
         }
