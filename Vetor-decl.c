9b6bd392eca1520fcf64df27bd2b80b76e74e7cf
Monique <moniquejroliveira@gmail.com>
2025-04-02 23:02:47 -0300

    Declarando notas com vetores em for

--- Vetor-decl.c
+++ Vetor-decl.c
@@ -3,12 +3,33 @@
 int main ()
 {
 
+    /*
     int vetor[5] = {}; //chaves vazia é p/ inserir 0 no resultado ou em algum vetor
 
     for (int i = 0; i < 5; ++i) {
         printf("vetor: %i\n", vetor[i]);
     }
+    */
 
+
+    float notas[5] = {0};
+    float total = 0;
+    float media = 0;
+
+    printf("Insira 5 notas:\n");
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%f", &notas[i]);
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        total += notas[i]; //adicionar na var TOTAL o valor inserido em NOTAS cada vez que o ciclo roda
+    }
+    
+    media = total/ 5;
+
+    printf("A media do aluno é: %.2f\n", media); //delimitar casa decimal
 
     return 0;
 }
