45021564f74f14528d4a3ab8d2c4c1c14ba3ebff
Monique <moniquejroliveira@gmail.com>
2025-04-02 22:48:20 -0300

    Laço for com vetor

---
+++ Vetor-decl.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+
+    int vetor[5] = {}; //chaves vazia é p/ inserir 0 no resultado ou em algum vetor
+
+    for (int i = 0; i < 5; ++i) {
+        printf("vetor: %i\n", vetor[i]);
+    }
+
+
+    return 0;
+}
