4687dd970210798544a799607619263dac950790
Monique <moniquejroliveira@gmail.com>
2025-04-02 22:36:28 -0300

    Vetores inteiros

---
+++ Vetor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+{
+
+    int num[5];
+
+    num[0] = 5;
+    num[1] = 7;
+    num[2] = 9;
+    num[3] = 4;
+    num[4] = 0;
+
+    printf("%i\n", num[2]);
+    
+
+    return 0;
+}
